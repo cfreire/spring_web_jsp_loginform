90ca74b96395588047e4b3514c1a45f177238a79
César Bento Freire <cfreire70@gmail.com>
2020-06-04 12:50:52 +0100

    Update to include spring boot with springweb

---
+++ src/main/java/com/vantagem/spring_web_jsp_validanif/SpringWebJspValidanifApplication.java
@@ -0,0 +1,13 @@
+package com.vantagem.spring_web_jsp_validanif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebJspValidanifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebJspValidanifApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vantagem/spring_web_jsp_validanif/SpringWebJspValidanifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vantagem.spring_web_jsp_validanif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebJspValidanifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
